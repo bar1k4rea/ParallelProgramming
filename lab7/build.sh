8a6ecd4b71a3083856433c668893371dd1f8f5f0
Barabanov Andrey <bbarabanow@ya.ru>
2022-11-24 16:33:55 +0300

    not ready build.sh

--- lab7/build.sh
+++ lab7/build.sh
@@ -2,13 +2,18 @@
 
 N_START=1
 N_END=100
-THREADS_NUMBER=2
+THREADS_NUMBER=6
+PROCESSORS_NUMBER=12
 
 echo "Compiling..."
 mpicc -o prog7 -lm src/lab7.c
 
 echo "Working..."
-mpirun -nq $THREADS_NUMBER ./prog7 $N_START $N_END
+mpirun -nq $THREADS_NUMBER ./prog7 $N_START $N_END $THREADS_NUMBER $PROCESSORS_NUMBER
+
+# TODO: graphics in Python
+
+# TODO: report in Latex
 
 echo "Deleting..."
 rm prog7
