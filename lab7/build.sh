4dd0c3131405817f36c0820d7b4df3d158eb3525
Barabanov Andrey <bbarabanow@ya.ru>
2022-11-24 19:49:22 +0300

    add graphics

--- lab7/build.sh
+++ lab7/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 
-N_START=1
-N_END=100
+N_START=100000000
+N_END=300000000
 THREADS_NUMBER=6
 PROCESSORS_NUMBER=12
 
@@ -11,10 +11,8 @@ mpicc -o prog7 -lm src/lab7.c
 echo "Working..."
 mpirun -nq $THREADS_NUMBER ./prog7 $N_START $N_END $THREADS_NUMBER $PROCESSORS_NUMBER
 
-# TODO: graphics in Python
-
-# TODO: report in Latex
+python3 src/plot.py
 
 echo "Deleting..."
-rm prog7
+rm prog7 timing.txt
 
